6a236f2e0421c2d98ce8974548c8b5fb737cb8fa
unknown <samsmusa@outlook.com>
2021-05-26 02:16:58 +0600

    initial commit

--- space_rocks/game.py
+++ space_rocks/game.py
@@ -1,9 +1,12 @@
 import pygame
 
+from utils import load_sprite
+
 class SpaceRocks:
     def __init__(self):
         self._init_pygame()
         self.screen = pygame.display.set_mode((800, 600))
+        self.background = load_sprite("sp", False)
 
     def main_loop(self):
         while True:
@@ -16,11 +19,13 @@ class SpaceRocks:
         pygame.display.set_caption("Space Rocks")
 
     def _handle_input(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPW):
+                quit()
 
     def _process_game_logic(self):
         pass
 
     def _draw(self):
-        self.screen.fill((0, 0, 255))
+        self.screen.blit(self.background, (0, 0))
         pygame.display.flip()
